1d0e7b23b11bc5c4025970e4b222f98a6d163aac
Merci <habmercima@gmail.com>
2023-03-22 07:38:27 +0900

    initial adjustments

---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Heading(){
+    return(
+        <div >
+            <h1 className="heading"> Telling Stories With Photos</h1>
+        </div>
+    );
+
+}
+
+export default Heading;
