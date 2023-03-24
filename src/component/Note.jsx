74fb6c5319bd1a53834c3571f3e0de9b3b14e7ab
Merci <habmercima@gmail.com>
2023-03-24 13:22:33 +0900

    view photos feature

--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -10,27 +10,25 @@ import image7 from"./photos/7.JPG";
 import image8 from"./photos/8.JPG";
 import image9 from"./photos/9.JPG";
 
-var currentImage = 1;
-var  image_ = 'image1';
-
-function afterClick(){
-    return (
-        currentImage === 9 ? 1 : (currentImage + 1),
-        image_ = "image" + { currentImage},
-        console.log(image_)
-    );
-}
-
 function Notes(){
+    const [currentImage, setChangeCount] = useState(1);
+    let picture;
+    function changePhoto(){
+        return (
+            currentImage === 9 ? 1 : (currentImage + 1),
+            setChangeCount( currentImage)
+        );
+    }
     return(
         
         <div className="note">
             <h1>Gallery</h1>
             <div className="photos">
-                <img class="img-responsive img-portfolio img-hover" src={image1} alt="art 1"/>
+                {  picture = "image" + {currentImage}};
+                <img class="img-responsive img-portfolio img-hover" src={picture} alt="art 1"/>
             </div>
             <button
-                    onClick={afterClick}
+                    onClick={changePhoto}
                     title="Next"
                     color="brown"> Next
                 </button>
